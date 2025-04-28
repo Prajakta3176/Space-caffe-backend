de69db86be0c0d2632037fa229d076df93513979
prajakta <prajaktamondhe3@gmail.com>
2025-04-28 21:38:58 +0530

    DEleted bcrypt and installed bcryptjs

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 import Admin from "../models/admin.js";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import validator from 'validator'
+import validator from "validator";
 
 export const adminSignup = async (req, res) => {
   try {
@@ -20,46 +20,49 @@ export const adminSignup = async (req, res) => {
 
     await newAdmin.save();
 
-    res.status(201).json({ message: "Admin registered successfully", id: newAdmin.id });
+    res
+      .status(201)
+      .json({ message: "Admin registered successfully", id: newAdmin.id });
   } catch (err) {
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const adminSignin = async (req, res) => {
-        try{
-            const {email, password } = req.body;
-            if(!(email && password) ){
-                return res.status(300).json({message: "All fields are required!"})
-            }
-    
-            const existingAdmin = await Admin.findOne({email : email});
-           
-    
-            if(!existingAdmin){
-                return res.status(400).json({success:false, message : "Invalid credential"})
-            }
-    
-    
-            const isMatch = await bcrypt.compare(password, existingAdmin.password);
-    
-            if(isMatch){
-                const authClaims =  {
-                    id : existingAdmin.id,
-                    role : "admin"
-                  }
-                         
-                  const token = jwt.sign( authClaims , process.env.JWT_PASSWORD, {
-                    expiresIn: "15d",
-                  });
-    
-                  res.status(200).json({success: true, id: existingAdmin.id, token: token })
-            }else{
-                return res.status(400).json({ message: "Invalid Credential" });
-            }
-    
-        }catch(err){
-            console.log(err);
-            res.status(500).json({success:false , message : "Internal server error"})   
-        }
+  try {
+    const { email, password } = req.body;
+    if (!(email && password)) {
+      return res.status(300).json({ message: "All fields are required!" });
+    }
+
+    const existingAdmin = await Admin.findOne({ email: email });
+
+    if (!existingAdmin) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credential" });
+    }
+
+    const isMatch = await bcrypt.compare(password, existingAdmin.password);
+
+    if (isMatch) {
+      const authClaims = {
+        id: existingAdmin.id,
+        role: "admin",
+      };
+
+      const token = jwt.sign(authClaims, process.env.JWT_PASSWORD, {
+        expiresIn: "15d",
+      });
+
+      res
+        .status(200)
+        .json({ success: true, id: existingAdmin.id, token: token });
+    } else {
+      return res.status(400).json({ message: "Invalid Credential" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 };
